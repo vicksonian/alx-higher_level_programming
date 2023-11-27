56a8d91f1a0fcef4e22ed9a22bbca9833733f9f6
George Michael Pussl <george.pussl@gmail.com>
2023-11-27 13:52:04 -0800

    0x00-python-hello_world

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -9,18 +9,17 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
+ * 
  */
-
 typedef struct listint_s
 {
     int n;
     struct listint_s *next;
 } listint_t;
 
-int check_cycle(listint_t *list);
 size_t print_listint(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
-listint_t *add_nodeint(listint **head, const int n);
-
+int check_cycle(listint_t *list);
 
 #endif /* LISTS_H */
