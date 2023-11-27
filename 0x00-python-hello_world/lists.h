5817469caf8cefbea03a2f7eeca9d47c5d34ca2c
George Michael Pussl <george.pussl@gmail.com>
2023-11-27 11:52:30 -0800

    updates

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,7 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stddef.h>
+#include <stdlib.h>
 
 /**
  * struct listint_s - singly linked list
@@ -10,6 +10,7 @@
  *
  * Description: singly linked list node structure
  */
+
 typedef struct listint_s
 {
     int n;
@@ -17,5 +18,9 @@ typedef struct listint_s
 } listint_t;
 
 int check_cycle(listint_t *list);
+size_t print_listint(const listint_t *h);
+void free_listint(listint_t *head);
+listint_t *add_nodeint(listint **head, const int n);
+
 
 #endif /* LISTS_H */
