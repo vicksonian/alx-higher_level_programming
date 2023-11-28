132a242877f5454d3c78a26ab5f950096ee88608
George Michael Pussl <george.pussl@gmail.com>
2023-11-28 09:44:53 -0800

    updates

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,13 +1,5 @@
 #!/usr/bin/python3
-
 def print_last_digit(number):
-    last_digit = abs(number) % 10
-    print(last_digit, end="")
-    return last_digit
-
-if __name__ == "__main__":
-    print_last_digit(98)
-    print_last_digit(0)
-    r = print_last_digit(-1024)
-    print(r)
-
+    """Print the last digit of a number and return it."""
+    print(abs(number) % 10, end="")
+    return (abs(number) % 10)
