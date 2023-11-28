44deafad282bb8b411efad875082a08d561c6578
George Michael Pussl <george.pussl@gmail.com>
2023-11-28 09:26:45 -0800

    updates

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,11 +3,9 @@
 def uppercase(str):
     for char in str:
         uppercase_char = char
+
         if ord(char) >= 97 and ord(char) <= 122:
+
             uppercase_char = chr(ord(char) - 32)
         print("{}".format(uppercase_char), end="")
     print()
-
-if __name__ == "__main__":
-    uppercase("best")
-    uppercase("Best School 98 Battery street")
