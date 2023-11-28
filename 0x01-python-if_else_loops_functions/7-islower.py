2201079182e7019dc90345c5b4865e92f0f7bfc1
George Michael Pussl <george.pussl@gmail.com>
2023-11-28 11:21:46 -0800

    updates

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -4,12 +4,11 @@ def islower(c):
     """Check if a character is lowercase."""
     return ord('a') <= ord(c) <= ord('z')
 
-if __name__ == "__main__":
-    islower = __import__('7-islower').islower
+    if __name__ == "__main__":
+        islower = __import__('7-islower').islower
 
-    print("a is {}".format("lower" if islower("a") else "upper"))
-    print("H is {}".format("lower" if islower("H") else "upper"))
-    print("A is {}".format("lower" if islower("A") else "upper"))
-    print("3 is {}".format("lower" if islower("3") else "upper"))
-    print("g is {}".format("lower" if islower("g") else "upper"))
-
+        print("a is {}".format("lower" if islower("a") else "upper"))
+        print("H is {}".format("lower" if islower("H") else "upper"))
+        print("A is {}".format("lower" if islower("A") else "upper"))
+        print("3 is {}".format("lower" if islower("3") else "upper"))
+        print("g is {}".format("lower" if islower("g") else "upper"))
