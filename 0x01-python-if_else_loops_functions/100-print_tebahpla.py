be682a0ea3f29b136b5f146cdf4f332e5726591e
George Michael Pussl <george.pussl@gmail.com>
2023-11-28 11:05:05 -0800

    updates

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 for i in range(ord('z'), ord('a') - 1, -1):
-    print("{:c}".format(i - 32) if i % 2 == 0 else "{:c}".format(i), end="")
+    print("{:c}".format(i) if (ord('z') - i) % 2 == 0 else "{:c}".format(i - 32), end="")
