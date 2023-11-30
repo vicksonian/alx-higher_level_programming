739411703de5b7468fff2177e46e54723068f6b7
George Michael Pussl <george.pussl@gmail.com>
2023-11-30 15:42:54 -0800

    update

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,9 +1,8 @@
-def magic_calculation(a, b):
-    add, sub = __import__('magic_calculation_102', fromlist=('add', 'sub'))
+#!/usr/bin/python3
+def magic_calculation(a, b, c):
+    """Match bytecode provided by Holberton School."""
     if a < b:
-        c = add(a, b)
-        for i in range(4, 6):
-            c = add(c, i)
-        return c
-    else:
-        return sub(a, b)
+        return (c)
+    if c > b:
+        return (a + b)
+    return (a*b - c)
