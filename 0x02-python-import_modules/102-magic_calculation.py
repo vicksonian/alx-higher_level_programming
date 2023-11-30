8f4370821e00a3a018d22eb442c0cbf4cdb698b2
George Michael Pussl <george.pussl@gmail.com>
2023-11-30 15:27:17 -0800

    updatea

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,5 +1,5 @@
 def magic_calculation(a, b):
-    add, sub = __import__('magic_calculation_102', fromlist=('add', 'sub'))
+    add, sub = magic_calculation_102.add, magic_calculation_102.sub
 
     if a < b:
         c = add(a, b)
@@ -8,5 +8,5 @@ def magic_calculation(a, b):
             c = add(c, i)
 
         return c
-    else:
-        return sub(a, b)
+
+    return sub(a, b)
