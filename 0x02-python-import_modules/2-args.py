2c018d9e62c55d3af020b1cb07741ce4093966fb
George Michael Pussl <george.pussl@gmail.com>
2023-11-30 15:08:24 -0800

    updates

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 from sys import argv
 
-length = len(argv) - 1
-if length == 0:
-    print("0 arguments.")
-elif length == 1:
-    print("1 argument:")
-else:
-    print("{} arguments:".format(length))
+if __name__ == "__main__":
+    argc = len(argv) - 1
+    args = argv[1:]
 
-for i in range(1, length + 1):
-    print("{}: {}".format(i, argv[i]))
+    if argc == 0:
+        print("0 arguments.")
+    else:
+        plural_s = "s" if argc > 1 else ""
+        print("{} argument{}:".format(argc, plural_s))
+
+        for i, arg in enumerate(args, 1):
+            print("{}: {}".format(i, arg))
