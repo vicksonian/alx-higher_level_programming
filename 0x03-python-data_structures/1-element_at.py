7df6495b0eabaaa770c5b42a061799a0c526b05c
George Michael Pussl <george.pussl@gmail.com>
2023-12-03 13:56:58 -0800

    fixing identation

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,11 +2,10 @@
 
 def element_at(my_list, idx):
     if idx < 0 or idx >= len(my_list):
-        return None
-    return my_list[idx]
+        return (None)
+    return (my_list[idx])
 
-# Example usage
-        if __name__ == "__main__":
+    if __name__ == "__main__":
         my_list = [1, 2, 3, 4, 5]
         idx = 3
         result = element_at(my_list, idx)
