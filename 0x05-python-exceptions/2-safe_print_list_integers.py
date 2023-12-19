41ed6ee724a3f29743a3c552302404087466ab49
George Michael Pussl <george.pussl@gmail.com>
2023-12-18 16:24:57 -0800

    0x05-python-exceptions

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,27 +1,21 @@
 #!/usr/bin/python3
 
 def safe_print_list_integers(my_list=[], x=0):
-    count = 0
+    """Print the first x elements of a list that are integers.
 
-    try:
-        for i in range(x):
-            if isinstance(my_list[i], int):
-                print("{:d}".format(my_list[i]), end="")
-                count += 1
-    except IndexError:
-        pass
+    Args:
+        my_list (list): The list to print elements from.
+        x (int): The number of elements of my_list to print.
 
-    print()
-    return (count)
-
-    if __name__ == "__main__":
-        my_list = [1, 2, 3, 4, 5]
-        nb_print = safe_print_list_integers(my_list, 2)
-        print("nb_print: {:d}".format(nb_print))
-
-        my_list = [1, 2, 3, "School", 4, 5, [1, 2, 3]]
-        nb_print = safe_print_list_integers(my_list, len(my_list))
-        print("nb_print: {:d}".format(nb_print))
-
-        nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
-        print("nb_print: {:d}".format(nb_print))
+    Returns:
+        The number of elements printed.
+    """
+    ret = 0
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            ret += 1
+        except (ValueError, TypeError):
+            continue
+    print("")
+    return (ret)
