c51a85a66747ed3dc4d265774189b8a4734d42af
George Michael Pussl <george.pussl@gmail.com>
2024-01-05 12:37:01 -0800

    0x09-python-everything_is_object

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,12 @@
+
+
+100-magic_string.py#!/usr/bin/python3
+def magic_string():
+    magic_string.counter = getattr(magic_string, 'counter', 0) + 1
+    return "BestSchool" + (", BestSchool" * (magic_string.counter - 1))
+
+
+#!/usr/bin/python3
+def magic_string():
+    magic_string.counter = getattr(magic_string, 'counter', 0) + 1
+    return "BestSchool" + (", BestSchool" * (magic_string.counter - 1))
