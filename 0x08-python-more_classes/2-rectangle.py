a4cd67cf3cf988b751f5b41fc0ec2ad8b81833b2
George Michael Pussl <george.pussl@gmail.com>
2024-01-04 16:01:41 -0800

    Real definition of a rectangle

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -34,5 +34,4 @@ class Rectangle:
         return self.__width * self.__height
 
     def perimeter(self):
-        return 0 if self.__width == 0 or self.__height == 0 else 2 * (self.__width + self.__height)
-
+    return 0 if self.__width == 0 or self.__h
