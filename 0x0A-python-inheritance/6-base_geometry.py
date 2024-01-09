e5094dbb94516d3c61ede172e9a64ee1f2fb45d0
George Michael Pussl <george.pussl@gmail.com>
2024-01-08 17:00:56 -0800

    Integer validator

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""module with class BaseGeometry"""
+
+
+class BaseGeometry:
+    """This is a BaseGeometry class"""
+
+    @classmethod
+    def area(self):
+        """This is a method for calculating area"""
+
+        raise Exception("area() is not implemented")
