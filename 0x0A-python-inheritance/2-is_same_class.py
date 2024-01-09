f2ae510bebd37bfb7d2215c0cb3cda6a4271fca4
George Michael Pussl <george.pussl@gmail.com>
2024-01-08 16:47:54 -0800

    updates

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+"""module with method is_same_class"""
 
 
 def is_same_class(obj, a_class):
-    """ This is a Method that return True if an object is an instance of a class"""
+    """Method that return True if an object is an instance of a class"""
+
     return type(obj) is a_class
