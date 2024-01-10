5cf8b9528326a5a461406f524123bf6ed5a5be88
George Michael Pussl <george.pussl@gmail.com>
2024-01-09 16:36:07 -0800

    updates

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-
-
+# 6-from_json_string.py
+"""Defines a JSON-to-object function."""
 import json
 
 
 def from_json_string(my_str):
+    """ function that returns an Python data represented by a JSON """
     return json.loads(my_str)
