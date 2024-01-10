ea8ee6ee98dc0b4495484844b45240f9e0cb0ba7
George Michael Pussl <george.pussl@gmail.com>
2024-01-09 16:33:40 -0800

    updates

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """Defines a JSON file-writing function."""
-
-
 import json
 
 
 def save_to_json_file(my_obj, filename):
-    with open(filename, "w", encoding="UTF-8") as f:
+    """function that writes an Object to a text file, using a JSON"""
+    with open(filename, "w") as f:
         json.dump(my_obj, f)
